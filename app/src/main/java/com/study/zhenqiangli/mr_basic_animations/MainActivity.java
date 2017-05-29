1a1b0aa6c83fd28a9e04268a79bf9ed55e0b94e3
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-05-29 15:08:49 -0400

    add main/detail activity for the animation

--- app/src/main/java/com/study/zhenqiangli/mr_basic_animations/MainActivity.java
+++ app/src/main/java/com/study/zhenqiangli/mr_basic_animations/MainActivity.java
@@ -1,13 +1,25 @@
 package com.study.zhenqiangli.mr_basic_animations;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.mid_cell);
+        imageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getBaseContext(), DetailActivity.class);
+        startActivity(intent);
     }
 }
