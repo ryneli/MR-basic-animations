3de223b7f823ed09fe844790ca4b655b710d647b
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-05-29 18:50:55 -0400

    add main -> detail enter transition

--- app/src/main/java/com/study/zhenqiangli/mr_basic_animations/DetailActivity.java
+++ app/src/main/java/com/study/zhenqiangli/mr_basic_animations/DetailActivity.java
@@ -2,12 +2,22 @@ package com.study.zhenqiangli.mr_basic_animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.animation.AnimationUtils;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Slide slide = new Slide(Gravity.BOTTOM);
+        slide.addTarget(R.id.description);
+        slide.setInterpolator(
+                AnimationUtils.loadInterpolator(this,
+                        android.R.interpolator.linear_out_slow_in));
+        slide.setDuration(500);
+        getWindow().setEnterTransition(slide);
         setContentView(R.layout.activity_detail);
     }
 }
